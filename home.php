33f3e87d9ab68bae15c16c6972331e33b875f21c
Vaibhav Rathore <vaibhav@coloredcow.in>
2017-03-27 20:23:34 +0530

    single.php and link

--- home.php
+++ home.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 
-<div class="blogs-container">
+<div class="blogs-page-container">
 	<div class="container">
-		<div class="blog-row">
+		<div class="row">
 		<?php
 			if(have_posts()):
 				while(have_posts()):
 					the_post();
 		?>
-				<div class="blog-block ">
-					<div class="title"><?php the_title(); ?></div>
-					<div class="date"><?php the_time('F d, Y'); ?></div>
-					<div class="excerpt"><?php the_excerpt(); ?></div>
-				</div>
+			<a href="<?php the_permalink(); ?>" class="blog-block ">
+				<div class="title"><?php the_title(); ?></div>
+				<div class="date"><?php the_time('F d, Y'); ?></div>
+				<div class="excerpt"><?php the_excerpt(); ?></div>
+			</a>
 		<?php
 				endwhile;
 			endif;
--- single.php
+++ single.php
@@ -1,12 +1,31 @@
-<?php
-/**
- * Template for displaying all single posts
- *
- * @package ColoredCow
- */
+<?php get_header(); ?>
 
-get_header(); ?>
-
-	
-
+<div class="page-single-container">
+	<div class="container">
+	<?php
+		if(have_posts()):
+			while(have_posts()):
+				the_post();
+	?>
+				<div class="row title">
+					<div class="col-xs-12">
+						<?php the_title(); ?>
+					</div>	
+				</div>
+				<div class="row date">
+					<div class="col-xs-12">
+						<?php the_time('F d, Y'); ?>
+					</div>	
+				</div>
+				<div class="row content">
+					<div class="col-xs-12">
+						<?php the_content(); ?>
+					</div>	
+				</div>
+	<?php
+			endwhile;
+		endif;
+	?>
+	</div>
+</div>
 <?php get_footer(); ?>
